4ec24af9ced85407d226a3bd66adcb08b7b36a2d
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2019-02-04 15:07:26 +0200

    Header

--- gulpfile.js
+++ gulpfile.js
@@ -98,5 +98,5 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "js", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "js", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
